28c672fb928c984a1c39e6829e1a20fc2d727044
Andraz Valentincic <andrazvalentincic@yahoo.com>
2025-03-25 13:29:53 +0100

    add schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from app.schemas.auth import LoginRequest, Token
+from app.schemas.user import User, UserBase, UserCreate, UserUpdate
+
+__all__ = ["User", "UserCreate", "UserUpdate", "UserBase", "Token", "LoginRequest"]
